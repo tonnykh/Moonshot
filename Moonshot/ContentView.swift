ef72c948c881f5f8fe6bc728d5e110189530613a
Tonnykh <tonnykhumbongmayum@gmail.com>
2023-06-25 23:28:22 +0530

    Load image and mission name on ContentView

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -8,16 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
-    
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
     
+    let columns = [GridItem(.adaptive(minimum: 150))]
+    
+    
     var body: some View {
-        VStack {
-            Text("\(astronauts.count)")
-            Text("\(missions.count)")
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            Text("Details view of \(mission.displayName)")
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                Text(mission.displayName)
+                                    .font(.headline)
+                                Text(mission.launchDate ?? "N/A")
+                                    .font(.caption)
+                            }
+                            .frame(maxWidth: .infinity)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Moonshot")
         }
-        .padding()
     }
 }
 

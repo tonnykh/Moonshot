f329737a65b449cba998cccd3aaff1a8535825d5
Tonnykh <tonnykhumbongmayum@gmail.com>
2023-06-25 22:48:59 +0530

    Add a Bundle extension to find, load & decode JSON files

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Khumbongmayum Tonny on 25/06/23.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}

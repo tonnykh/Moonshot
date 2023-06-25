c29c5f7b3d662861e3fa8f66e848acdf930467d4
Tonnykh <tonnykhumbongmayum@gmail.com>
2023-06-25 22:12:29 +0530

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Khumbongmayum Tonny on 25/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

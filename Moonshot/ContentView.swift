4f4c7dc60a9d00d8ea7cdd4ad6f0ccc3ce1adf35
Tonnykh <tonnykhumbongmayum@gmail.com>
2023-06-25 23:43:19 +0530

    Update the mission date to a easily readable date

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -29,7 +29,7 @@ struct ContentView: View {
                                     .frame(width: 100, height: 100)
                                 Text(mission.displayName)
                                     .font(.headline)
-                                Text(mission.launchDate ?? "N/A")
+                                Text(mission.formattedLaunchDate)
                                     .font(.caption)
                             }
                             .frame(maxWidth: .infinity)
